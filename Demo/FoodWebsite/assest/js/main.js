0213e607ce60252e8c3c460c151b9dee8b78722d
Rahida Mahmudova <86527275+mahmudovarahide@users.noreply.github.com>
2021-10-07 10:05:05 +0300

    Delete Demo/FoodWebsite/assest directory

--- Demo/FoodWebsite/assest/js/main.js
+++
@@ -1,8 +0,0 @@
-
-window.addEventListener("scroll",function(){
-	if(this.pageYOffset>60){
-		document.querySelector(".navbar").classList.add("sticky");
-	}else{
-		document.querySelector(".navbar").classList.remove("sticky");
-	}
-});
